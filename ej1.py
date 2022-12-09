e48f980d8e372752707fb6b4d469d87d7b14bb56
valuvilla <valuvp@hotmail.com>
2022-12-09 11:11:30 +0100

    intento1

--- ej1.py
+++ ej1.py
@@ -3,7 +3,24 @@ def minion_game(string):
     stuart=0
     vocal=["a","e","i","o","u"]
     for i in range(0,len(string)):
-        if string[i] in vocal:
+        palabra=string[:i]
+        if vocal in :
+            kevin+=string.count(palabra)
+            print(kevin)
+        else:
+            stuart+=string.count(palabra)
+    print(kevin)
+    if kevin>stuart:
+        return("Kevin",kevin)
+    else:
+        return("Stuart",stuart)
+
+
+print(minion_game("BANANA"))
+
+    
+
+
             
 
-if __name__ == '__main__':
+#if __name__ == '__main__':
