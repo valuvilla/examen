37a75916bab7afde87cdb9edc5bc8851b2b1d140
valuvilla <valuvp@hotmail.com>
2022-12-09 12:00:15 +0100

    banana

--- banana.py
+++ banana.py
@@ -7,18 +7,28 @@ def minion_game(string):
     # Stuart forma palabras que comiencen con consonantes
         if string[i] not in "AEIOU":
          # Iteramos sobre todas las subcadenas que comiencen con la letra actual
-         for j in range(i + 1, len(string) + 1):
-            # Agregamos la subcadena al diccionario y aumentamos su puntuación en 1
-            substrings[string[i:j]] = substrings.get(string[i:j], 0) + 1
+            for j in range(i + 1, len(string) + 1):
+                # Agregamos la subcadena al diccionario y aumentamos su puntuación en 1
+                kevin=substrings[string[i:j]] = substrings.get(string[i:j], 0) + 1
+            
 
 
         # Kevin forma palabras que comiencen con vocales
         else:
         # Iteramos sobre todas las subcadenas que comiencen con la letra actual
-         for j in range(i + 1, len(string) + 1):
+            for j in range(i + 1, len(string) + 1):
             # Agregamos la subcadena al diccionario y aumentamos su puntuación en 1
-            substrings[string[i:j]] = substrings.get(string[i:j], 0) + 1
+                stuart=substrings[string[i:j]] = substrings.get(string[i:j], 0) + 1
 
+    # Si la puntuación de Kevin es mayor que la de Stuart, Kevin gana
+    if kevin>stuart:
+        print("Kevin",kevin)
+    # Si la puntuación de Stuart es mayor que la de Kevin, Stuart gana
+    elif stuart>kevin:
+        print("Stuart",stuart)
+    # Si la puntuación de Kevin y Stuart es igual, es un empate
+    else:
+        print("Empate")
     
     return None
 
